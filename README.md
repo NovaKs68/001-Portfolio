47367fbea3ef4a4b61a26f187b5a62c9d5e7214e
NovaKs68 <willemintom03@gmail.com>
2020-11-06 16:48:32 +0100

    Ajout de la biography animé et d'un début de documentation.

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # 001-Portfolio
+
+Ce projet consiste à développer un portfolio.
+
+Il a plusieurs buts :
+ * Apprendre à initialiser un projet avec une grosse architecture
+ * Approfondir mes connaissances en Angular, en NodeJs/Express et en BDD.
+ * Apprendre l'hébergement (auto-hébergé)
+ * Avoir un site de démonstration
+ * Découvrir beaucoup de nouveaux concepts
--- client/src/app/biography-menu/biography-menu.component.ts
+++ client/src/app/biography-menu/biography-menu.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, HostListener, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-biography-menu',
@@ -6,14 +6,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./biography-menu.component.scss']
 })
 export class BiographyMenuComponent implements OnInit {
+
   imgSlideRobotHexapod: string;
   imgSlideServerNAS: string;
+  showPresentMe: boolean;
+
   constructor() {
     this.imgSlideRobotHexapod = 'assets/img/RobotHexapodSlide.PNG'
     this.imgSlideServerNAS = 'assets/img/ServerNASSlide.JPG'
   }
 
-  ngOnInit(): void {
+  ngOnInit(): void {  }
+
+  @HostListener('window:scroll', ['$event'])
+  scrollHandler(event) {
+    console.log("Scroll Event");
+    let winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+    let height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+    let scrolled = (winScroll / height) * 100;
+    console.log(scrolled)
+    if(scrolled > 15) {
+      this.showPresentMe = true;
+    }
   }
 
 }
