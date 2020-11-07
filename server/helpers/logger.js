230dd699ab8ad7625754ed8b6e0d97d9aebcd87b
NovaKs68 <willemintom03@gmail.com>
2020-11-07 21:27:56 +0100

    Ajout du mode production avec docker (pm2 pour le back et nginx pour le front)

---
+++ server/helpers/logger.js
@@ -0,0 +1,23 @@
+require('../config/database');
+const winston = require('winston');
+require('winston-daily-rotate-file');
+
+const transport = new (winston.transports.DailyRotateFile)({
+    filename: './server/log/%DATE%.log',
+    datePattern: 'MMDD',
+    zippedArchive: true,
+    maxFiles: '7d',
+    level: 'debug',
+
+});
+
+
+const logger = winston.createLogger({
+    format: winston.format.combine(
+        winston.format.timestamp(),
+        winston.format.printf(i => `${i.timestamp} | ${i.level} | ${i.message}`)
+    ),
+    transports: [
+        transport
+    ]
+});
