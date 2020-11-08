dd7c3dc748e370d99d845b1e63a25b51273c3b0d
tom.willemin <willemintom03@gmail.com>
2020-11-08 10:03:34 +0100

    Ajout de la table Project. Avec quelques données de developpement. Par la suite il faudra la lier a une table de photos pour avoir plusieurs photos par projet.

--- BDD/dev-init.d/1-BDD-Portfolio-tables.sql
+++ BDD/dev-init.d/1-BDD-Portfolio-tables.sql
@@ -2,3 +2,15 @@ DROP DATABASE IF EXISTS portfolio;
 CREATE DATABASE IF NOT EXISTS portfolio;
 USE portfolio;
 
+CREATE USER IF NOT EXISTS 'tomwillemin'@'%' IDENTIFIED BY 'tom';
+GRANT ALL PRIVILEGES ON portfolio . * TO 'tomSQL'@'%';
+
+----------------------Table Projects----------------------
+CREATE TABLE projects (
+        id_project SMALLINT NOT NULL AUTO_INCREMENT,
+        title_project VARCHAR(255) NOT NULL,
+        picture_project MEDIUMBLOB,
+        resume_project VARCHAR(255) NOT NULL,
+        PRIMARY KEY (id_project)
+)
+Engine = INNODB;
